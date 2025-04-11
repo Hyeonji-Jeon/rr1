5969327eed16b724b7cc020755cb3c7d920a76ee
Hyeonji Jeon <guswl1397@naver.com>
2025-04-11 15:48:43 +0900

    todo

--- app/components/todo/listComponent.tsx
+++ app/components/todo/listComponent.tsx
@@ -1,7 +1,27 @@
+import {useQuery} from "@tanstack/react-query";
+import {testTodoList} from "~/api/todoAPI";
+
+
 function TodoListComponent (){
+
+    const query = useQuery({
+        queryKey: ['todos'],
+        queryFn: async() => {
+
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            const todos:Todo[] = await testTodoList()
+
+            return todos
+        }
+    })
+
+    const {isFetching, data, error} = query
+
     return(
         <div>
             <div className={'text-4xl'}>Todo List Component</div>
+            <div className={'text-3xl bg-amber-600'}>{isFetching && <h1>Loading..........</h1>}</div>
         </div>
     )
 }
