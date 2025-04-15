d1eb336486e9a33e3b67bc25ff619d3be7a83e1e
Hyeonji Jeon <guswl1397@naver.com>
2025-04-15 15:21:01 +0900

    todo

--- app/routes/member/kakaoRedirect.tsx
+++ app/routes/member/kakaoRedirect.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSearchParams} from "react-router";
+import {getAccessToken} from "~/api/kakaoAPI";
 
 function KakaoRedirect() {
 
     const [searchParams] = useSearchParams()
 
     const authCode = searchParams.get('code')
+
+    useEffect(() => {
+
+        if(authCode){
+            getAccessToken(authCode).then(data => {
+                const accessToken = data.access_token;
+                console.log(accessToken);
+            });
+        }
+    }, [authCode])
 
     if(!authCode){
         return (<div>로그인 실패</div>)
