2b7d3ea7d95b197a4fe63589554c4080aff6524a
Hyeonji Jeon <guswl1397@naver.com>
2025-04-15 14:25:29 +0900

    todo

--- app/api/kakaoAPI.ts
+++ app/api/kakaoAPI.ts
@@ -3,6 +3,8 @@ const redirect_uri = 'http://localhost:5173/member/kakao'
 
 const auth_code_path = `https://kauth.kakao.com/oauth/authorize`
 
+const access_token_url = 'https://kauth.kakao.com/oauth/token'
+
 export const getKakaoLoginLink = () => {
 
     const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
